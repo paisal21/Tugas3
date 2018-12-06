c71400cb28e2e2da376047171bfecff022cef049
paisal <45650351+paisal21@users.noreply.github.com>
2018-12-06 13:55:44 +0700

    Create MainStackGeneric.java

---
+++ MainStackGeneric.java
@@ -0,0 +1,13 @@
+package ASD3;
+
+public class MainStackGeneric {
+	public static void main(String[] args) {
+		StackGeneric<Integer> st = new StackGeneric<Integer>();
+        st.push(2);
+        st.push(1);
+        st.push(2);
+        st.cetak();
+        System.out.println("SIZE: " + st.size());
+        System.out.println(st.pop());
+    }
+}

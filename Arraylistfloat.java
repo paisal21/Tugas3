855e3b39a5bbc7d2217448c04beee7272a40ffbb
paisal <45650351+paisal21@users.noreply.github.com>
2018-12-06 13:35:24 +0700

    Create Arraylistfloat.java

---
+++ Arraylistfloat.java
@@ -0,0 +1,16 @@
+package ASD3;
+
+import java.util.ArrayList;
+public class Arraylistfloat {
+
+	
+	public static void main(String[] args) {
+		ArrayList<Float>isi = new ArrayList<Float>();
+		isi.add(1.2f);
+		isi.add(2.3F);
+		isi.add(3.4F);
+		System.out.println("arraylistfloat : "+isi.toString());
+		System.out.println("SIZE : "+isi.size());
+
+	}
+}

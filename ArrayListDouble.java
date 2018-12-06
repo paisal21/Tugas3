4f981344445928dbad830c6ffceefa3aa1589060
paisal <45650351+paisal21@users.noreply.github.com>
2018-12-06 13:34:40 +0700

    Create ArrayListDouble.java

---
+++ ArrayListDouble.java
@@ -0,0 +1,17 @@
+package ASD3;
+
+import java.util.ArrayList;
+public class ArrayListDouble {
+
+	
+	public static void main(String[] args) {
+		ArrayList<Double>isi = new ArrayList<Double>();
+		isi.add(1.2);
+		isi.add(2.3);
+		isi.add(3.4);
+		System.out.println("arraylistdouble : "+isi.toString());
+		System.out.println("SIZE : "+isi.size());
+
+	}
+
+}

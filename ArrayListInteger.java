4bcf05c9c25962291f3503e663b004daf7115133
paisal <45650351+paisal21@users.noreply.github.com>
2018-12-06 13:37:06 +0700

    Create ArrayListInteger.java

---
+++ ArrayListInteger.java
@@ -0,0 +1,14 @@
+package ASD3;
+mport java.util.ArrayList;
+
+public class ArrayListInteger {
+
+	public static void main(String[] args) {
+		ArrayList<Integer>lint = new ArrayList<Integer>();
+		lint.add(2);
+		lint.add(1);
+		lint.add(2);
+		System.out.println("MASIH ADA"+lint.toString());
+		System.out.println("SIZE : "+lint.size());	
+	}
+}

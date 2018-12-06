ca4a24af204c520fe20101425b08593ed68759be
paisal <45650351+paisal21@users.noreply.github.com>
2018-12-06 13:53:41 +0700

    Create MainStuckDouble.java

---
+++ MainStuckDouble.java
@@ -0,0 +1,20 @@
+package ASD3;
+
+public class MainStuckDouble {
+public static void main(String[] args) {
+		classstackdouble sc=new classstackdouble();
+		sc.push(2.3);
+		sc.cetak();
+		sc.push(0.0);
+		
+		sc.cetak();
+		
+		double d0=sc.pop();
+		System.out.println(d0);
+		double d1=sc.pop();
+		System.out.println(d1);
+		sc.cetak();
+		double d2=sc.pop();
+		System.out.println("kosong lah");
+	}
+}
